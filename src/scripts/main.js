41e65942d818c93cbf6bcf6086bdcf93f10489aa
Adm <adm@teste.com>
2025-01-15 20:19:25 -0300

    Envio de arquivos do exercicio grunt

---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    ducument.getElementById('form-sorteador').addEventListener('submit', function () {
+        let numeroMaximo = ducument.getElementById('numero-maximo');
+        numeroMaximo = parseInt(numeroMaximo);
+    })
+})
